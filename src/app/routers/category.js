4d447d7e2f08d74d95ac3e0a23c3eb1626f6b8da
william ribeiro <william_jag_rs@hotmail.com>
2021-03-24 17:54:14 -0300

    create router categoryr

--- src/app/routers/category.js
+++ src/app/routers/category.js
@@ -6,7 +6,7 @@ const routes = new Router();
 routes.get('/category', CategoryController.index);
 routes.get('/category/:id', CategoryController.show);
 routes.post('/category', CategoryController.store);
-// routes.put('/category/:id', CategoryController.update);
-// routes.delete('/category/:id', CategoryController.delete);
+routes.put('/category/:id', CategoryController.update);
+routes.delete('/category/:id', CategoryController.delete);
 
 export default routes;
