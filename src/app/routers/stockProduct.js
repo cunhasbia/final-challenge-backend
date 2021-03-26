1ed7a9bf939d172478a1c720b4be22b3d00c51c3
william ribeiro <william_jag_rs@hotmail.com>
2021-03-26 13:49:46 -0300

    finished route stockProduct

--- src/app/routers/stockProduct.js
+++ src/app/routers/stockProduct.js
@@ -4,5 +4,9 @@ import StockProductController from '../controllers/StockProductController';
 const routes = new Router();
 
 routes.get('/stock-product', StockProductController.index);
+routes.get('/stock-product/:id', StockProductController.show);
+routes.post('/stock-product', StockProductController.store);
+routes.put('/stock-product/:id', StockProductController.update);
+routes.delete('/stock-product/:id', StockProductController.delete);
 
 export default routes;
