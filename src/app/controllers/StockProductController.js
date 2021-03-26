cf2158e6ea6242943065e417f71ad5a0a4a5e2aa
william ribeiro <william_jag_rs@hotmail.com>
2021-03-26 13:49:18 -0300

    finished StockProductController

--- src/app/controllers/StockProductController.js
+++ src/app/controllers/StockProductController.js
@@ -8,10 +8,9 @@ class StockProductController {
   async index(request, response) {
     try {
       const stockProduct = await StockProduct.findAll({
-        attributes: ['id'],
-
+        attributes: ['id', 'quantity'],
         include: [
-          { model: Product, as: 'products', attributes: ['name'] },
+          { model: Product, as: 'products', attributes: ['id', 'name'] },
           { model: Stock, as: 'stock', attributes: ['name'] },
         ],
       });
@@ -21,10 +20,168 @@ class StockProductController {
       return response.status(error.status || 400).json(error.message);
     }
   }
-  // async show(request, response) {}
-  // async store(request, response) {}
-  // async update(request, response) {}
-  // async delete(request, response) {}
+
+  async show(request, response) {
+    try {
+      const { id } = request.params;
+
+      const parsed = Number.parseInt(id);
+
+      if (Number.isNaN(parsed)) {
+        return response.status(400).json({ message: 'Invalid ID' });
+      }
+
+      const stockProduct = await StockProduct.findOne({
+        where: { id },
+        attributes: ['id', 'quantity'],
+        include: [
+          { model: Product, as: 'products', attributes: ['name'] },
+          { model: Stock, as: 'stock', attributes: ['name'] },
+        ],
+      });
+
+      if (!stockProduct) {
+        return response.status(404).json({ message: 'Stock not found' });
+      }
+
+      return response.json(stockProduct);
+    } catch (error) {
+      return response.status(error.status || 400).json(error.message);
+    }
+  }
+
+  async store(request, response) {
+    try {
+      const { quantity, product_id, stock_id } = request.body;
+
+      if (!quantity || !product_id || !stock_id) {
+        return response.status(400).json({ message: 'Invalid data' });
+      }
+
+      const parsedProduct = Number.parseInt(product_id);
+      const parsedStock = Number.parseInt(stock_id);
+
+      if (Number.isNaN(parsedProduct)) {
+        return response.status(400).json({ message: 'Invalid ID' });
+      }
+
+      if (Number.isNaN(parsedStock)) {
+        return response.status(400).json({ message: 'Invalid ID' });
+      }
+
+      const product = await Product.findByPk(parsedProduct);
+
+      if (!product) {
+        return response.status(404).json({ message: 'Product not found' });
+      }
+
+      const stock = await Stock.findByPk(parsedStock);
+
+      if (!stock) {
+        return response.status(404).json({ message: 'Stock not found' });
+      }
+
+      // faz a soma e inclui na tabela produtos
+      const quantityTotal = product.total;
+      product.total = quantityTotal + quantity;
+      product.save();
+
+      const stockProduct = await StockProduct.create({
+        quantity,
+        product_id,
+        stock_id,
+      });
+
+      return response.json({ stockProduct });
+    } catch (error) {
+      return response.status(error.status || 400).json(error.message);
+    }
+  }
+
+  async update(request, response) {
+    try {
+      const { id } = request.params;
+      const { quantity, stock_id } = request.body;
+
+      if (!quantity || !stock_id) {
+        return response.status(400).json({ message: 'Invalid data' });
+      }
+      const parsedId = Number.parseInt(id);
+      const parsedStock = Number.parseInt(stock_id);
+
+      if (Number.isNaN(parsedId)) {
+        return response.status(400).json({ message: 'Invalid ID' });
+      }
+
+      if (Number.isNaN(parsedStock)) {
+        return response.status(400).json({ message: 'Invalid ID' });
+      }
+
+      const stockProduct = await StockProduct.findByPk(parsedId);
+
+      if (!stockProduct) {
+        return response.status(404).json({
+          message: 'Product stock not found',
+        });
+      }
+
+      const product = await Product.findByPk(stockProduct.product_id);
+
+      if (!product) {
+        return response.status(404).json({ message: 'Product not found' });
+      }
+
+      const stock = await Stock.findByPk(parsedStock);
+
+      if (!stock) {
+        return response.status(404).json({ message: 'Stock not found' });
+      }
+
+      // faz a soma e inclui na tabela produtos
+      const quantityTotal = product.total;
+      product.total = quantityTotal - stockProduct.quantity + quantity;
+      product.save();
+
+      stockProduct.stock_id = parsedStock;
+      stockProduct.quantity = quantity;
+      stockProduct.save();
+
+      return response.json(product);
+    } catch (error) {
+      return response.status(error.status || 400).json(error.message);
+    }
+  }
+
+  async delete(request, response) {
+    try {
+      const { id } = request.params;
+      const parsedId = Number.parseInt(id);
+
+      if (Number.isNaN(parsedId)) {
+        return response.status(400).json({ message: 'Invalid ID' });
+      }
+
+      const stockProduct = await StockProduct.findByPk(parsedId);
+
+      if (!stockProduct) {
+        return response.status(404).json({
+          message: 'Product stock not found',
+        });
+      }
+      const product = await Product.findByPk(stockProduct.product_id);
+
+      // subtrai e inclui na tabela produtos
+      const quantityTotal = product.total;
+      product.total = quantityTotal - stockProduct.quantity;
+      product.save();
+
+      stockProduct.destroy();
+
+      return response.sendStatus(202);
+    } catch (error) {
+      return response.status(error.status || 400).json(error.message);
+    }
+  }
 }
 
 export default new StockProductController();
