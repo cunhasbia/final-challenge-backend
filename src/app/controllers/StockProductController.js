0d005ebc58774abcededb4eeda95e936c7f248e9
william ribeiro <william_jag_rs@hotmail.com>
2021-03-26 09:46:39 -0300

    start StockProductController

--- src/app/controllers/StockProductController.js
+++ src/app/controllers/StockProductController.js
@@ -1,13 +1,30 @@
 /* eslint-disable radix */
 /* eslint-disable camelcase */
-// import StockProduct from '../models/StockProduct';
+import StockProduct from '../models/StockProduct';
+import Stock from '../models/Stock';
+import Product from '../models/Product';
 
-class ProductController {
-  // async index(request, response) {}
+class StockProductController {
+  async index(request, response) {
+    try {
+      const stockProduct = await StockProduct.findAll({
+        attributes: ['id'],
+
+        include: [
+          { model: Product, as: 'products', attributes: ['name'] },
+          { model: Stock, as: 'stock', attributes: ['name'] },
+        ],
+      });
+
+      return response.json(stockProduct);
+    } catch (error) {
+      return response.status(error.status || 400).json(error.message);
+    }
+  }
   // async show(request, response) {}
   // async store(request, response) {}
   // async update(request, response) {}
   // async delete(request, response) {}
 }
 
-export default new ProductController();
+export default new StockProductController();
