f73a7a93edb64f35bea52ed69b53dccb86bb6d13
Lucimara Rios Nakata <67387775+lurnakata@users.noreply.github.com>
2021-03-27 10:26:28 -0300

    - add route saleReturn

--- src/app/routers/index.js
+++ src/app/routers/index.js
@@ -4,6 +4,7 @@ import product from './product';
 import stock from './stock';
 import stockProduct from './stockProduct';
 import sale from './sale';
+import saleReturn from './saleReturn';
 import swagger from './swagger';
 
-export default [category, reason, product, stock, stockProduct, sale, swagger];
+export default [category, reason, product, stock, stockProduct, sale, saleReturn, swagger];
