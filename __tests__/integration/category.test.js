bc9cab9249de7f4c0f1dba3118795310bf8831c0
Bianca Cunha <biancunha@gmail.com>
2021-04-01 14:42:53 -0300

    adjusting integration tests

--- __tests__/integration/category.test.js
+++ __tests__/integration/category.test.js
@@ -3,7 +3,7 @@ import app from '../../src/app';
 
 describe('category', () => {
   describe('store', () => {
-    it('should create a new category', async () => {
+    it('should be able to create a new category', async () => {
       expect.assertions(2);
 
       const response = await request(app).post('/category').send({
@@ -13,14 +13,14 @@ describe('category', () => {
       expect(response.status).toBe(200);
       expect(response.body).toHaveProperty('id');
     });
-    it('should return a error when send a invalid data', async () => {
+    it('should not be able to create a new category when sending invalid data', async () => {
       expect.assertions(1);
 
       const response = await request(app).post('/category').send({
         name: '',
       });
 
-      expect(response.status).toBe(404);
+      expect(response.status).toBe(400);
     });
   });
 
@@ -33,27 +33,26 @@ describe('category', () => {
       expect(response.status).toBe(200);
       expect(response.body).toHaveProperty('count');
     });
-    it('should return a error when send invalid data as pagination', async () => {
+    it('should be able to filter a category by name', async () => {
       expect.assertions(1);
-
-      const response = await request(app).get('/category?limit=a&page=b');
-
-      expect(response.status).toBe(400);
-    });
-    it('should filter a category by name', async () => {
-      expect.assertions(2);
 
       const response = await request(app).get(
         '/category?limit=100&page=1&name=Science'
       );
 
       expect(response.status).toBe(200);
-      expect(response.body).toHaveProperty('count');
+    });
+    it('should not be able to list all categories when sending invalid data as pagination', async () => {
+      expect.assertions(1);
+
+      const response = await request(app).get('/category?limit=a&page=b');
+
+      expect(response.status).toBe(400);
     });
   });
 
   describe('show', () => {
-    it('should show a category with the id passed as parameter', async () => {
+    it('should show a category when sending id as parameter', async () => {
       expect.assertions(2);
 
       const response = await request(app).get('/category/1?limit=100&page=1');
@@ -61,7 +60,7 @@ describe('category', () => {
       expect(response.status).toBe(200);
       expect(response.body).toHaveProperty('id');
     });
-    it('should return a error when sending a string as id param', async () => {
+    it('should not be able to show a category when sending a string as id parameter', async () => {
       expect.assertions(1);
 
       const response = await request(app).get('/category/a?limit=100&page=1');
--- __tests__/integration/sale.test.js
+++ __tests__/integration/sale.test.js
@@ -3,23 +3,20 @@ import app from '../../src/app';
 
 describe('sale', () => {
   describe('store', () => {
-    it('should create a new sale', async () => {
+    it('should be able to create a new sale', async () => {
       expect.assertions(3);
 
       const category = await request(app).post('/category').send({
-        name: 'Fiction',
+        name: 'Fantasy',
       });
-
       const product = await request(app).post('/product').send({
         name: 'Book - The Lord of the Rings',
         price: 45,
         category_id: category.body.id,
       });
-
       const stock = await request(app).post('/stock').send({
         name: 'RS',
       });
-
       const stockProduct = await request(app).post('/stock-product').send({
         quantity: 100,
         product_id: product.body.id,
@@ -36,6 +33,17 @@ describe('sale', () => {
       expect(response.body).toHaveProperty('id');
       expect(response.body).toHaveProperty('quantity');
     });
+    it('should not be able to create a new sale when sending invalid data', async () => {
+      expect.assertions(1);
+
+      const response = await request(app).post('/sale').send({
+        quantity: '',
+        product_id: '',
+        stock_id: '',
+      });
+
+      expect(response.status).toBe(400);
+    });
   });
 
   describe('index', () => {
@@ -49,7 +57,7 @@ describe('sale', () => {
   });
 
   describe('show', () => {
-    it('should show a specific sale when send id as route param', async () => {
+    it('should show a specific sale when sending id as parameter', async () => {
       expect.assertions(3);
 
       const response = await request(app).get('/sale/1');
@@ -58,7 +66,7 @@ describe('sale', () => {
       expect(response.body).toHaveProperty('id');
       expect(response.body).toHaveProperty('quantity');
     });
-    it('should return a error when sending a invalid data as id param', async () => {
+    it('should not be able to show a sale when sending a string as id parameter', async () => {
       expect.assertions(1);
 
       const response = await request(app).get('/sale/a');
