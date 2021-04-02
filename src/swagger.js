7fb9a2d481760ab45faff19975581ee2f6072095
ThiellyLanger <71675011+ThiellyLanger@users.noreply.github.com>
2021-04-02 20:20:35 -0300

    Add files via upload
    att

--- src/swagger.js
+++ src/swagger.js
@@ -3,9 +3,13 @@ const swaggerConfig = require('./config/swagger');
 
 const outputFile = './src/swagger-documentation.json';
 const endpoints = [
-  './src/app/routers/user.js',
-  './src/app/routers/auth.js',
+  './src/app/routers/category.js',
   './src/app/routers/product.js',
+  './src/app/routers/reason.js',
+  './src/app/routers/sale.js',
+  './src/app/routers/saleReturn.js',
+  './src/app/routers/stock.js',
+  './src/app/routers/stockProduct.js',
 ];
 
 swaggerAutogen(outputFile, endpoints, swaggerConfig);
